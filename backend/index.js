64aaecbfc7e30eb7a30f41c39b952049c5a25a03
Rascal_Two <therealrascaltwo@gmail.com>
2023-01-13 22:48:51 -0500

    Connect to database before starting server

--- backend/index.js
+++ backend/index.js
@@ -15,111 +15,112 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/databa
 const DB_NAME = MONGODB_URI.split('/').at(-1).split('?')[0];
 
 
-let db;
+console.log(`Connecting to MongoDB "${DB_NAME}"...`)
 MongoClient.connect(MONGODB_URI).then(client => {
-	db = client.db(DB_NAME);
-});
+	const db = client.db(DB_NAME);
+	console.log(`Connected to MongoDB "${DB_NAME}".`);
 
-//#region Passport
+	//#region Passport
 
-passport.use(new LocalStrategy((username, password, done) => {
-	db.collection('users').findOne({ username }).then(user => {
-		if (!user) {
-			return done(null, false, { message: 'Incorrect username.' });
-		}
-		if (user.password !== password) {
-			return done(null, false, { message: 'Incorrect password.' });
-		}
-		return done(null, user);
-	}).catch(err => done(err));
-}));
-
-passport.serializeUser((user, done) => done(null, user._id.toString()));
-passport.deserializeUser((id, done) =>
-	db.collection('users').findOne({ _id: ObjectId(id) }).then(user => {
-		done(null, user);
-	}).catch(err => done(err))
-);
-
-//#endregion Passport
-//#region Express Middlewares
-
-const app = express();
-
-app.use(express.static(path.join('public')));
-app.use(express.json());
-app.use(cors({
-	origin: 'http://127.0.0.1:5173',
-	credentials: true,
-}));
-
-app.use(session({
-	secret: 'secret',
-	resave: false,
-	saveUninitialized: true,
-	store: MongoStore.create({ mongoUrl: MONGODB_URI })
-}));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-
-//#endregion Express Middlewares
-//#region Auth Routes
-
-app.post('/api/user/login', passport.authenticate('local', { successRedirect: '/api/user', failureRedirect: '/api/user', failureFlash: true }));
-app.post('/api/user/signup', async (request, response, next) => {
-	try {
-		const { username, password } = request.body;
-		const existingUser = await db.collection('users').findOne({ username, password });
-		if (existingUser) {
-			return response.json({ user: null, messages: { error: ['User already exists']} });
-		}
-		const user = await db.collection('users').insertOne({ username, password });
-		request.login(user, (err) => {
-			if (err) {
-				return next(err);
+	passport.use(new LocalStrategy((username, password, done) => {
+		db.collection('users').findOne({ username }).then(user => {
+			if (!user) {
+				return done(null, false, { message: 'Incorrect username.' });
 			}
-			return response.json({ user });
-		});
-	} catch (err) {
-		next(err);
-	}
-});
+			if (user.password !== password) {
+				return done(null, false, { message: 'Incorrect password.' });
+			}
+			return done(null, user);
+		}).catch(err => done(err));
+	}));
 
-app.get('/api/user', (request, response) =>
-	response.send({
-		user: request.user || null,
-		messages: request.flash()
-	})
-);
+	passport.serializeUser((user, done) => done(null, user._id.toString()));
+	passport.deserializeUser((id, done) =>
+		db.collection('users').findOne({ _id: ObjectId(id) }).then(user => {
+			done(null, user);
+		}).catch(err => done(err))
+	);
 
-//#endregion Auth Routes
-//#region Counter Routes
+	//#endregion Passport
+	//#region Express Middlewares
 
-app.route('/api/counter')
-	.get(async (request, response, next) => {
+	const app = express();
+
+	app.use(express.static(path.join('public')));
+	app.use(express.json());
+	app.use(cors({
+		origin: 'http://127.0.0.1:5173',
+		credentials: true,
+	}));
+
+	app.use(session({
+		secret: 'secret',
+		resave: false,
+		saveUninitialized: true,
+		store: MongoStore.create({ mongoUrl: MONGODB_URI })
+	}));
+	app.use(flash());
+	app.use(passport.initialize());
+	app.use(passport.session());
+
+	//#endregion Express Middlewares
+	//#region Auth Routes
+
+	app.post('/api/user/login', passport.authenticate('local', { successRedirect: '/api/user', failureRedirect: '/api/user', failureFlash: true }));
+	app.post('/api/user/signup', async (request, response, next) => {
 		try {
-			const counter = await db.collection('counters').findOne({ owner: ObjectId(request.user._id) });
-			return response.send({ count: counter?.count ?? 0 });
-		} catch (err) {
-			next(err);
-		}
-	})
-	.post(async (request, response, next) => {
-		try {
-			let counter = await db.collection('counters').findOne({ owner: ObjectId(request.user._id) }) || {
-				_id: ObjectId(),
-				count: 0
-			};
-
-			counter = (await db.collection('counters').findOneAndUpdate({ owner: ObjectId(request.user._id) }, { $set: { count: counter.count + +request.body.change } }, { upsert: true, returnDocument: 'after' })).value;
-
-			return response.send({ count: counter.count });
+			const { username, password } = request.body;
+			const existingUser = await db.collection('users').findOne({ username, password });
+			if (existingUser) {
+				return response.json({ user: null, messages: { error: ['User already exists']} });
+			}
+			const user = await db.collection('users').insertOne({ username, password });
+			request.login(user, (err) => {
+				if (err) {
+					return next(err);
+				}
+				return response.json({ user });
+			});
 		} catch (err) {
 			next(err);
 		}
 	});
 
-//#endregion Counter Routes
+	app.get('/api/user', (request, response) =>
+		response.send({
+			user: request.user || null,
+			messages: request.flash()
+		})
+	);
 
-app.listen(PORT, () => console.log(`Listening at https://localhost:${PORT}/`));
+	//#endregion Auth Routes
+	//#region Counter Routes
+
+	app.route('/api/counter')
+		.get(async (request, response, next) => {
+			try {
+				const counter = await db.collection('counters').findOne({ owner: ObjectId(request.user._id) });
+				return response.send({ count: counter?.count ?? 0 });
+			} catch (err) {
+				next(err);
+			}
+		})
+		.post(async (request, response, next) => {
+			try {
+				let counter = await db.collection('counters').findOne({ owner: ObjectId(request.user._id) }) || {
+					_id: ObjectId(),
+					count: 0
+				};
+
+				counter = (await db.collection('counters').findOneAndUpdate({ owner: ObjectId(request.user._id) }, { $set: { count: counter.count + +request.body.change } }, { upsert: true, returnDocument: 'after' })).value;
+
+				return response.send({ count: counter.count });
+			} catch (err) {
+				next(err);
+			}
+		});
+
+	//#endregion Counter Routes
+
+	app.listen(PORT, () => console.log(`Listening at https://localhost:${PORT}/`));
+});
